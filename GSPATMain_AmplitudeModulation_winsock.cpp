ad490aaae1ed413123373c69b5f934f04049208f
YongjiangChen <Y.Chen-317@sms.ed.ac.uk>
2024-09-10 11:14:18 +0100

    fixed a bug

--- GSPATMain_AmplitudeModulation_winsock.cpp
+++ GSPATMain_AmplitudeModulation_winsock.cpp
@@ -279,7 +279,7 @@ void listenForUpdates() {
                 }
             }
             else {
-                threadSafeCout("Received invalid position data");
+                threadSafeCout("Received invalid position data or pet outside the camera view");
             }
         }
         else if (iResult == 0) {
